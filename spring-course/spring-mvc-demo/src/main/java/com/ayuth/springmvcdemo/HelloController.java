04de4ac3fa7baccb3aa3243bf1619d5b3561f474
Ayuth Mangmesap <ayuth.mang@gmail.com>
2019-11-12 02:11:24 +0700

    :tada: prepared project for spring MVC in Section 12 :tada:

---
+++ spring-course/spring-mvc-demo/src/main/java/com/ayuth/springmvcdemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.ayuth.springmvcdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "main-menu";
+    }
+}
---
+++ spring-course/spring-mvc-demo/src/main/java/com/ayuth/springmvcdemo/SpringMvcDemoApplication.java
@@ -0,0 +1,15 @@
+package com.ayuth.springmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDemoApplication.class, args);
+    }
+
+}
