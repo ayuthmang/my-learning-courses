1ae74c99c30396b4da86035300493d82b209a769
Ayuth Mangmesap <ayuth.mang@gmail.com>
2020-04-10 14:33:17 +0700

    VueMastery - Mastering Vuex: State & Getters

--- vue-mastery/beginner-mastering-vuex/real-world-vue-lesson11-vuex-start/src/store.js
+++ vue-mastery/beginner-mastering-vuex/real-world-vue-lesson11-vuex-start/src/store.js
@@ -4,7 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal walfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => state.categories.length,
+    doneTodos: state => state.todos.filter(todo => todo.done),
+    activeTodosCount: state => state.todos.filter(todo => !todo.done).length,
+    getTodoById: state => id => state.todos.find(todo => todo.id === id)
+  }
 })
--- vue-mastery/beginner-mastering-vuex/real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
+++ vue-mastery/beginner-mastering-vuex/real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>There are {{ catLength }} categories.</p>
+    <p>{{ $store.getters.doneTodos.length }}</p>
+    <p>{{ $store.getters.activeTodosCount }}</p>
+    <p>{{ getTodoById(1) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['catLength', 'getTodoById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
