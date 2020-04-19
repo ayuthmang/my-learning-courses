0829c36475a62714ef4b50bf3cee0a461b221631
Ayuth Mangmesap <ayuth.mang@gmail.com>
2020-04-19 14:32:52 +0700

    GraphQL finished 'Working with Arrays: Part II' and before

--- udemy-graphql-course/graphql-basics/src/index.js
+++ udemy-graphql-course/graphql-basics/src/index.js
@@ -2,13 +2,55 @@ import { GraphQLServer } from "graphql-yoga";
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [
+  {
+    id: "1",
+    name: "Ayuth",
+    email: "ayuth@example.com",
+    age: 23,
+  },
+  {
+    id: "2",
+    name: "Sarah",
+    email: "Sarah@example.com",
+    age: 22,
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "Mike@example.com",
+    age: null,
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "Learn GraphQL",
+    body: "Find some place to learn GraphQL.",
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Learn JavaScript",
+    body: "Find some place to learn JavaScript.",
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Learn How to Love People",
+    body: "Give love to everyone.",
+    published: true,
+  },
+];
+
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    greeting(name: String, position: String): String!
-    add(a: Float!, b: Float!): Float!
+    users(query: String): [User!]!
     me: User!
-    post: Post!
+    posts(query: String): [Post!]!
   }
 
   type User {
@@ -29,20 +71,15 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      console.log(args);
-      if (args.name) {
-        return `Hello ${args.name}! You are my favourite ${args.position}`;
-      } else {
-        return `Hello`;
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
       }
-    },
-    add(parent, args) {
-      if (args.a && args.b) {
-        return `${args.a + args.b}`;
-      } else {
-        return `Please provide a and b.`;
-      }
+      return users.filter((user) => {
+        return user.name
+          .toLocaleLowerCase()
+          .includes(args.query.toLocaleLowerCase());
+      });
     },
     me() {
       return {
@@ -52,13 +89,17 @@ const resolvers = {
         age: 23,
       };
     },
-    post() {
-      return {
-        id: "12312121",
-        title: "The New York Times",
-        body: "Lorem ipsum",
-        published: false,
-      };
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter((post) => {
+        const isMatchTitle = post.title
+          .toLocaleLowerCase()
+          .includes(args.query);
+        const isMatchBody = post.body.toLocaleLowerCase().includes(args.query);
+        return isMatchTitle || isMatchBody;
+      });
     },
   },
 };
